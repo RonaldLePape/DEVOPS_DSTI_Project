4a6c597ccdfb8ccc01d6891cc576bf1a0d6aa8b2
Ronald LE PAPE <ronald.le-pape@edu.dsti.institute>
2025-06-07 18:41:31 +0200

    Pipeline test - Hello from AWS! - 2

--- UserApi/app/src/index.js
+++ UserApi/app/src/index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World from AWS!'))
 
 app.use('/user', userRouter)
 
